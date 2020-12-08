d762c4769ad65e98e551ed77dc36167d88aa7086
adam-94 <ashughes0777@gmail.com>
2020-12-08 17:04:19 +0000

    Updated import structure. Added bcrypt and login manager

--- Dashboard/__init__.py
+++ Dashboard/__init__.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_msearch import Search
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 import secrets
 
 app = Flask(__name__)
-secrect = secrets.token_urlsafe(32)
-app.secret_key = secrect
+secret = secrets.token_urlsafe(32)
+app.config['SECRET_KEY'] = secret
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['TESTING'] = True
+
+db = SQLAlchemy(app)
+search = Search()
+search.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
 
 from Dashboard import routes
