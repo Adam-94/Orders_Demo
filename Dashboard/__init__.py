fd9e11a232f0894ca120fd03a5b8a9789e6e4894
adam-94 <ashughes0777@gmail.com>
2020-12-08 23:05:44 +0000

    Added config to __init__

--- Dashboard/__init__.py
+++ Dashboard/__init__.py
@@ -3,22 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_msearch import Search
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from Dashboard.config import Config
 
 import secrets
 
 app = Flask(__name__)
-secret = secrets.token_urlsafe(32)
-app.config['SECRET_KEY'] = secret
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['TESTING'] = True
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 search = Search()
 search.init_app(app)
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+from Dashboard.main.routes import main
+from Dashboard.errors.handler import errors
 
-
-from Dashboard import routes
+app.register_blueprint(main)
+app.register_blueprint(errors)
