4b5a9a3b88c8e01f0bbfb68186e6d1d786e12dcd
adam-94 <ashughes0777@gmail.com>
2020-12-09 16:07:46 +0000

    Removed unused files

--- Dashboard/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
